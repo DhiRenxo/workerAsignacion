c919b904cd1bab9baff53da38cdd1209979a1374
DhiRenxo <cmezairiarte6@gmail.com>
2025-11-20 15:46:46 -0500

    se quita .env

--- app/config.py
+++ app/config.py
@@ -1,18 +1,25 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # Configuración de correo
     MAIL_USERNAME: str
     MAIL_PASSWORD: str
     MAIL_FROM: str
     MAIL_SERVER: str = "smtp.gmail.com"
     MAIL_PORT: int = 587
 
+    # Configuración MQTT
     MQTT_BROKER: str
     MQTT_PORT: int = 8884
     MQTT_USER: str
     MQTT_PASSWORD: str
     MQTT_TOPIC_EMAIL: str = "notificaciones/email"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    # Configuración para Pydantic
+    model_config = SettingsConfigDict(
+        env_file=".env",  # Solo se usa localmente
+        env_file_encoding="utf-8"
+    )
 
+# Instancia global
 settings = Settings()
